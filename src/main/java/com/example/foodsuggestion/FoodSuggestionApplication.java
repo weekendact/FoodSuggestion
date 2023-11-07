91f84026d1dc9a53e68d840d9bfbd0232e359039
weekendact <rjs740229@naver.com>
2023-11-07 09:59:35 +0900

    Initial commit

---
+++ src/main/java/com/example/foodsuggestion/FoodSuggestionApplication.java
@@ -0,0 +1,13 @@
+package com.example.foodsuggestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodSuggestionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodSuggestionApplication.class, args);
+    }
+
+}
